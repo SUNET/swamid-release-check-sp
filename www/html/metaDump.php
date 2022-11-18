dc384bb6815969ae1e11fea91b6be9bc62569348
Björn Mattsson <bjorn@sunet.se>
2022-11-18 08:43:57 +0100

    Addes som more tests in the dump for metadata.*swamid.se

--- www/html/metaDump.php
+++ www/html/metaDump.php
@@ -1,7 +1,7 @@
 <?php
 # Script to dump IdP:s info for import at metadata.swamid.se
 $db = new SQLite3("/var/www/tests/log/idpStatus");
-$testHandler = $db->prepare("SELECT * FROM idpStatus WHERE Test='cocov1-1' OR Test='rands';");
+$testHandler = $db->prepare("SELECT * FROM idpStatus WHERE Test='anonymous' OR Test='pseudonymous' OR Test='personalized' OR Test='cocov2-1' OR Test='cocov1-1' OR Test='rands';");
 
 $metaObj = new \stdClass();
 
@@ -14,6 +14,32 @@ while ($testResult=$testResults->fetchArray(SQLITE3_ASSOC)) {
 	$partObj->result = $testResult['TestResult'];
 	$entityArray[] = $partObj;
 	unset($partObj);
+}
+
+$testESIHandler = $db->prepare("SELECT * FROM idpStatus WHERE Test='esi' OR Test='esi-stud' ORDER BY Idp, Test DESC;");
+$testESIResults=$testESIHandler->execute();
+$ESITestResult = '';
+while ($testResult=$testESIResults->fetchArray(SQLITE3_ASSOC)) {
+	if ($testResult['Test'] == 'esi') {
+		if ($ESITestResult == '') {
+			$ESITime = $testResult['Time'];
+			$ESITestResult = $testResult['TestResult'];
+		} elseif ($ESITestResult <> 'schacPersonalUniqueCode OK') {
+			$ESITime = $testResult['Time'];
+			$ESITestResult = $testResult['TestResult'];
+		}
+		$partObj = new \stdClass();
+		$partObj->entityID = $testResult['Idp'];
+		$partObj->test = 'esi';
+		$partObj->time = $ESITime;
+		$partObj->result = $ESITestResult;
+		$entityArray[] = $partObj;
+		unset($partObj);
+		$ESITestResult = '';
+	} else {
+		$ESITime = $testResult['Time'];
+		$ESITestResult = $testResult['TestResult'];
+	}
 }
 
 $Obj = new \stdClass();

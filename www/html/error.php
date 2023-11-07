d49657c67eb42729b7ea0b56109f686f4a4f431e
Björn Mattsson <bjorn@sunet.se>
2023-11-07 16:00:36 +0100

    Fixed CSS bug in error.php

--- www/html/error.php
+++ www/html/error.php
@@ -1,4 +1,5 @@
 <?php
+include ("config.php");
 include ("include/header.php");
 
 $errorURL = isset($_GET['errorURL']) ? 'For more info visit this <a href="' . $_GET['errorURL'] . '">support-page</a>.' : '';
@@ -12,36 +13,17 @@ switch ($_GET['errorType']) {
 		showMetadataException();
 		break;
 	case 'opensaml::FatalProfileException' :
-		switch ($_GET['eventType']) {
-			case 'Login' :
-				switch ($_GET['statusCode']) {
-					case 'urn:oasis:names:tc:SAML:2.0:status:Responder' : 
-						if (isset($_GET['statusCode2'])) {
-							switch ($_GET['statusCode2']) {
-								//case 'urn:oasis:names:tc:SAML:2.0:status:AuthnFailed' :
-								//case 'urn:oasis:names:tc:SAML:2.0:status:NoPassive' :
-								case 'urn:oasis:names:tc:SAML:2.0:status:NoAuthnContext' :
-								//case 'urn:oasis:names:tc:SAML:2.0:status:RequestDenied' :
-									$errorURL = str_replace(array('ERRORURL_CODE', 'ERRORURL_CTX'), array('AUTHENTICATION_FAILURE', 'https://refeds.org/profile/mfa'), $errorURL);
-									break;
-							}
-						} 
-						break;
-				}
-				break;
+		if ($_GET['eventType'] == 'Login' && 
+            $_GET['statusCode'] == 'urn:oasis:names:tc:SAML:2.0:status:Responder' && 
+            isset($_GET['statusCode2']) && 
+            $_GET['statusCode2'] == 'urn:oasis:names:tc:SAML:2.0:status:NoAuthnContext') {
+                        //case 'urn:oasis:names:tc:SAML:2.0:status:AuthnFailed' :
+                        //case 'urn:oasis:names:tc:SAML:2.0:status:NoPassive' :
+                        //case 'urn:oasis:names:tc:SAML:2.0:status:RequestDenied' :
+            $errorURL = str_replace(array('ERRORURL_CODE', 'ERRORURL_CTX'), 
+                array('AUTHENTICATION_FAILURE', 'https://refeds.org/profile/mfa'), $errorURL);
 		}
 		showFatalProfileException();
-        
-		/*requestURL=https://release-check.swamid.se/Shibboleth.sso/SAML2/POST
-errorType=opensaml::FatalProfileException
-errorText=SAML response reported an IdP error.
-RelayState=https://release-check.swamid.se/result
-entityID=http://fs.bth.se/adfs/services/trust
-errorURL=https://error.swamid.se/?errorurl_code&ERRORURL_CODE%26errorurl_ts&ERRORURL_TS%26errorurl_rp&ERRORURL_RP%26errorurl_tid&ERRORURL_TID%26errorurl_ctx&ERRORURL_CTX%26entityid&http://fs.bth.se/adfs/services/trust
-eventType=Login
-statusCode=urn:oasis:names:tc:SAML:2.0:status:Responder
-statusCode2=urn:oasis:names:tc:SAML:2.0:status:RequestDenied
-*/
 		break;
 	default :
 		showInfo();
@@ -58,8 +40,8 @@ function showMetadataException() {?>
     </p>
     <p>Please include the following error message in any email:</p>
     <p class="error">Identity provider lookup failed at (<?=$_GET['requestURL']?>)</p>
-    <p><strong>EntityID:</strong> <?=$_GET['entityID']?></p>
-    <p><?=$_GET['errorType']?>: <?=$_GET['errorText']?></p>
+    <p><strong>EntityID:</strong> <?=htmlspecialchars($_GET['entityID'])?></p>
+    <p><?=htmlspecialchars($_GET['errorType'])?>: <?=htmlspecialchars($_GET['errorText'])?></p>
 <?php }
 
 function showFatalProfileException() {
@@ -71,11 +53,11 @@ function showFatalProfileException() {
     If your are the IdP administrator you can reach out to <a href="mailto:operations@swamid.se">operations@swamid.se</a>.
     </p>
     <p>Please include the following error message in any email:</p>
-    <p class="error">Identity provider lookup failed at (<?=$_GET['requestURL']?>)</p>
-    <p><strong>EntityID:</strong> <?=$_GET['entityID']?></p>
-    <p><?=$_GET['errorType']?>: <?=$_GET['errorText']?></p><?php
-    print isset($_GET['statusCode']) ? "\n<p>statusCode : " . $_GET['statusCode'] . '</p>' : '';
-    print isset($_GET['statusCode2']) ? "\n<p>statusCode2 : " . $_GET['statusCode2'] . '</p>' : '';
+    <p class="error">Identity provider lookup failed at (<?=htmlspecialchars($_GET['requestURL'])?>)</p>
+    <p><strong>EntityID:</strong> <?=htmlspecialchars($_GET['entityID'])?></p>
+    <p><?=htmlspecialchars($_GET['errorType'])?>: <?=htmlspecialchars($_GET['errorText'])?></p><?php
+    print isset($_GET['statusCode']) ? "\n<p>statusCode : " . htmlspecialchars($_GET['statusCode']) . '</p>' : '';
+    print isset($_GET['statusCode2']) ? "\n<p>statusCode2 : " . htmlspecialchars($_GET['statusCode2']) . '</p>' : '';
     #showInfo();
  }
 
@@ -83,11 +65,8 @@ function showInfo() { ?>
     <table>
     <?php
     foreach ($_GET as $key => $value) {
-            printf('<tr><td>%s = %s</td></tr>%s', $key, $value, "\n");
+            printf('<tr><td>%s = %s</td></tr>%s', $key, htmlspecialchars($value), "\n");
     }
     print "</table>";
-    #print "<pre>";
-    #print_r($_SERVER);
-    #print "</pre>";
     ?>
 <?php }

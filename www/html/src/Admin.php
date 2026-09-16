41d2f684998dab884b4fa41898846db3eb7f2b82
Björn Mattsson <bjorn@sunet.se>
2026-09-16 10:16:50 +0200

    fix: Split showUsageStat into 3 functions to reduce complexity

--- www/html/src/Admin.php
+++ www/html/src/Admin.php
@@ -914,70 +914,35 @@ class Admin
       default:
     }
     $last = date('Y-m-d');
+    $tomorow = date('Y-m-d', mktime(0, 0, 0, $month, $day + 1, $year));
 
     $idpList = '';
     $regAuthList = '';
 
     $testRuns = $this->config->getDb()->prepare(
       'SELECT COUNT(`testRuns`.`id`)
+      FROM `testRuns`
+      WHERE `testRuns`.`time` > :First
+        AND `testRuns`.`time` < :Last;'
+    );
+    $testIpds = $this->config->getDb()->prepare(
+      'SELECT COUNT(DISTINCT `idp_id`)
+      FROM `testRuns`
+      WHERE `testRuns`.`time` > :First
+        AND `testRuns`.`time` < :Last;'
+    );
+    $testFedrations = $this->config->getDb()->prepare(
+      'SELECT COUNT(DISTINCT `registrationAuthority`)
       FROM `testRuns`, `idps`
       WHERE `testRuns`.`time` > :First
         AND `testRuns`.`time` < :Last
         AND `idp_id` = `idps`.`id`;'
     );
-    $testIpdsHandler = $this->config->getDb()->prepare(
-      'SELECT `entityID`, `registrationAuthority`, MAX(`time`) AS lastRun, COUNT(`testRuns`.`id`) AS nrOfRuns
-      FROM `testRuns`, `idps`
-      WHERE `testRuns`.`time` > :First
-        AND `testRuns`.`time` < :Last
-        AND `idp_id` = `idps`.`id`
-      GROUP BY `entityID`
-      ORDER BY `entityID`;'
-    );
-    $testFedrations = $this->config->getDb()->prepare(
-      'SELECT DISTINCT `registrationAuthority`, MAX(`time`) AS lastRun, COUNT(`testRuns`.`id`) AS nrOfRuns
-      FROM `testRuns`, `idps`
-      WHERE `testRuns`.`time` > :First
-        AND `testRuns`.`time` < :Last
-        AND `idp_id` = `idps`.`id`
-      GROUP BY `registrationAuthority`
-      ORDER BY `registrationAuthority`;'
-    );
 
-    $testRuns->bindValue('First', $first . ' 00:00:01');
-    $testRuns->bindValue('Last', $last . ' 23:59:59');
-    $testIpdsHandler->bindValue('First', $first . ' 00:00:01');
-    $testIpdsHandler->bindValue('Last', $last . ' 23:59:59');
-    $testFedrations->bindValue('First', $first . ' 00:00:01');
-    $testFedrations->bindValue('Last', $last . ' 23:59:59');
-    $testRuns->execute();
-    $testIpdsHandler->execute();
-    $testFedrations->execute();
 
-    $idpCount = 0;
-    $regAuthCount = 0;
-    while ($idp = $testIpdsHandler->fetch(PDO::FETCH_ASSOC)) {
-      $idpList .= sprintf(
-        '                <tr><td>%s</td><td>%s</td><td  class="nowrap">%s</td><td>%s</td></tr>%s',
-        $idp['entityID'],
-        $idp['registrationAuthority'],
-        $idp['lastRun'],
-        $idp['nrOfRuns'],
-        "\n"
-      );
-      $idpCount++;
-    }
-
-    while ($idp = $testFedrations->fetch(PDO::FETCH_ASSOC)) {
-      $regAuthList .= sprintf(
-        '                <tr><td>%s</td><td>%s</td><td>%s</td></tr>%s',
-        $idp['registrationAuthority'],
-        $idp['lastRun'],
-        $idp['nrOfRuns'],
-        "\n"
-      );
-      $regAuthCount++;
-    }
+    $testRuns->execute(['First' => $first, 'Last' => $tomorow]);
+    $testIpds->execute(['First' => $first, 'Last' => $tomorow]);
+    $testFedrations->execute(['First' => $first, 'Last' => $tomorow]);
 
     printf(
       '      <div class="row">
@@ -990,13 +955,48 @@ class Admin
       '<a href="?tab=stats&show=week">' . _('Last Week') . '</a></p>
           <p>' . _('Showing betwen %s and %s') . '
           ' . _('<p>In this period, we had %d unique successful tests from %s IdPs') .
-      ' ' . _('belonging to %s Identity Federations') . '.</p>
-          <h3>
-            <i id="idpList-icon" class="fas fa-chevron-circle-%s"></i>
-            <a data-toggle="collapse" href="#idpList" aria-expanded="%s"' .
+      ' ' . _('belonging to %s Identity Federations') . '.</p>%s',
+      $first,
+      $last,
+      $testRuns->fetchColumn(),
+      $testIpds->fetchColumn(),
+      $testFedrations->fetchColumn(),
+      "\n"
+    );
+    $this->showUsageStatsIdPs($first, $tomorow);
+    $this->showUsageStatsRAs($first, $tomorow);
+    print "        </div><!-- End col-->
+      </div><!-- End row-->\n";
+  }
+  /**
+   * Show usage stats for IdP:s
+   *
+   * @param string $from
+   *
+   * @param string $to
+   *
+   * @return void
+   */
+  public function showUsageStatsIdPs($from, $to)
+  {
+    $testIpdsHandler = $this->config->getDb()->prepare(
+      'SELECT `entityID`, `registrationAuthority`, MAX(`time`) AS lastRun, COUNT(`testRuns`.`id`) AS nrOfRuns
+      FROM `testRuns`, `idps`
+      WHERE `testRuns`.`time` > :First
+        AND `testRuns`.`time` < :Last
+        AND `idp_id` = `idps`.`id`
+      GROUP BY `entityID`
+      ORDER BY `entityID`;'
+    );
+    $testIpdsHandler->execute(['First' => $from, 'Last' => $to]);
+
+    printf(
+      '          <h3>
+            <i id="idpList-icon" class="fas fa-chevron-circle-right"></i>
+            <a data-toggle="collapse" href="#idpList" aria-expanded="false"' .
       ' aria-controls="idpList">' . _('Idp List') . '</a>
           </h3>
-          <div class="collapse%s multi-collapse" id="idpList">
+          <div class="collapse multi-collapse" id="idpList">
             <table id="idpTable" class="table table-striped table-bordered">
               <thead>
                 <tr>
@@ -1007,28 +1007,56 @@ class Admin
                 </tr>
               </thead>
               <tbody>%s',
-      $first,
-      $last,
-      $testRuns->fetchColumn(),
-      $idpCount,
-      $regAuthCount,
-      "right", # ? "right" : "down",
-      'false',
-      '',
       "\n"
     );
-    print $idpList;
+    while ($idp = $testIpdsHandler->fetch(PDO::FETCH_ASSOC)) {
+      printf(
+        '                <tr><td>%s</td><td>%s</td><td  class="nowrap">%s</td><td>%s</td></tr>%s',
+        $idp['entityID'],
+        $idp['registrationAuthority'],
+        $idp['lastRun'],
+        $idp['nrOfRuns'],
+        "\n"
+      );
+    }
     printf(
       '              </tbody>
             </table>
-          </div><!-- end collapse -->
-          <h3>
-            <i id="regAuthList-icon" class="fas fa-chevron-circle-%s"></i>
-            <a data-toggle="collapse" href="#regAuthList" aria-expanded="%s"' .
+          </div><!-- end collapse -->%s',
+      "\n"
+    );
+  }
+
+  /**
+   * Show usage stats for RA:s
+   *
+   * @param string $from
+   *
+   * @param string $to
+   *
+   * @return void
+   */
+  public function showUsageStatsRAs($from, $to)
+  {
+    $testFedrationsHandler = $this->config->getDb()->prepare(
+      'SELECT DISTINCT `registrationAuthority`, MAX(`time`) AS lastRun, COUNT(`testRuns`.`id`) AS nrOfRuns
+      FROM `testRuns`, `idps`
+      WHERE `testRuns`.`time` > :First
+        AND `testRuns`.`time` < :Last
+        AND `idp_id` = `idps`.`id`
+      GROUP BY `registrationAuthority`
+      ORDER BY `registrationAuthority`;'
+    );
+    $testFedrationsHandler->execute(['First' => $from, 'Last' => $to]);
+
+    printf(
+      '          <h3>
+            <i id="regAuthList-icon" class="fas fa-chevron-circle-right"></i>
+            <a data-toggle="collapse" href="#regAuthList" aria-expanded="false"' .
       ' aria-controls="regAuthList">' . _('registrationAuthority List') . '</a>
           </h3>
-          <div class="collapse%s multi-collapse" id="regAuthList">
-            <table id="idpTable" class="table table-striped table-bordered">
+          <div class="collapse multi-collapse" id="regAuthList">
+            <table id="regAuthTable" class="table table-striped table-bordered">
               <thead>
                 <tr>
                   <th>' . _('registrationAuthority') . '</th>
@@ -1037,20 +1065,22 @@ class Admin
                 </tr>
               </thead>
               <tbody>%s',
-      "right", # ? "right" : "down",
-      'false',
-      '',
       "\n"
     );
-    print $regAuthList;
+    while ($ra = $testFedrationsHandler->fetch(PDO::FETCH_ASSOC)) {
+      printf(
+        '                <tr><td>%s</td><td>%s</td><td>%s</td></tr>%s',
+        $ra['registrationAuthority'],
+        $ra['lastRun'],
+        $ra['nrOfRuns'],
+        "\n"
+      );
+    }
     printf(
       '              </tbody>
             </table>
           </div><!-- end collapse -->%s',
       "\n"
     );
-
-    print "        </div><!-- End col-->
-      </div><!-- End row-->\n";
   }
 }
